1dc4af996dead749e85e8ffb36a86e4bd375f012
Kunal <kunal.singh.07007@gmail.com>
2025-09-26 19:45:51 +0530

    Before Zero error bug Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import UserInput from './Components/UserInput'
+import Results from './Components/Results'
 import Investimg from './assets/investment-calculator-logo.png'
+import { useState } from 'react'
 function App() {
+  const[userInput,setUserInput]=useState({
+    initialInvestment:10000,
+    annualInvestment:1200,
+    expectedReturn:7,
+    duration:10
+})
+const isValid=userInput.duration >=1;
+function handleChange(inputIdentifier,newValue){
+  setUserInput((prevInput)=>{
+      return{
+          ...prevInput,
+          [inputIdentifier]:+newValue
+      }
+  })
+}
   return (
+    
     <>
     <div id="header">
       <img src={Investimg} alt="" />
       <h1>Investment Calculator</h1>
     </div>
-    <UserInput/>
+    <UserInput userInput={userInput} onChange={handleChange}/>
+    {isValid ? <Results input={userInput}/>: <p className='center'>Please Enter Duration Greater than Zero.</p> }
     </>
     
   )
