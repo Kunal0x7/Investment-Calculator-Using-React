da164ee1237094fccc22e06fcb43dd18b62d2d76
Kunal <kunal.singh.07007@gmail.com>
2025-09-26 12:55:55 +0530

    Added UserInput.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import UserInput from './Components/UserInput'
 import Investimg from './assets/investment-calculator-logo.png'
 function App() {
   return (
+    <>
     <div id="header">
       <img src={Investimg} alt="" />
       <h1>Investment Calculator</h1>
     </div>
+    <UserInput/>
+    </>
+    
   )
 }
 
